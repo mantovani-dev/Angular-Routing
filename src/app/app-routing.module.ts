d65cc79055329347a59cffe87384d10941b3b7cc
André Mantovani <100172681+mantovani-dev@users.noreply.github.com>
2023-04-03 14:28:45 -0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {path: 'register', component: RegisterComponent },
-  {path: 'edit/:index', component: RegisterComponent }, // Usando o mesmo componente Register, envia um index como parâmetro
+  {path: 'edit/:index', component: RegisterComponent },
   {path: 'list', component: ListComponent }
 ];
 
